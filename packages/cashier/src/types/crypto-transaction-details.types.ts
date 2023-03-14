9a8a01dd957bd1109d0db0d6964b91de21dbaee6
Rostik <rostislav@deriv.com>
2023-03-14 13:32:18 +0300

    refactor: more importing types instead of defining whats defined

--- packages/cashier/src/types/crypto-transaction-details.types.ts
+++ packages/cashier/src/types/crypto-transaction-details.types.ts
@@ -1,23 +1,15 @@
+import { TTransactionType, TStatusCode } from './transactions.types';
+
 export type TCryptoTransactionDetails = {
     address_hash: string;
     address_url: string;
     amount: number;
     id: string;
     is_valid_to_cancel: number;
-    status_code:
-        | 'confirmed'
-        | 'error'
-        | 'pending'
-        | 'cancelled'
-        | 'locked'
-        | 'performing_blockchain_txn'
-        | 'processing'
-        | 'rejected'
-        | 'sent'
-        | 'verified';
+    status_code: TStatusCode;
     status_message: string;
     submit_date: number;
-    transaction_type: 'deposit' | 'withdrawal';
+    transaction_type: TTransactionType;
     transaction_hash: string;
     transaction_url: string;
 };
