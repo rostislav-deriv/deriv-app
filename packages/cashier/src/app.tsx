ff52a3c121423bff987e735da00f5cdb5472b031
Rostik Kayko <rostislav@deriv.com>
2023-04-11 16:36:40 +0300

    refactor: refactored app to ts

--- packages/cashier/src/app.tsx
+++ packages/cashier/src/app.tsx
@@ -3,8 +3,17 @@ import { setWebsocket } from '@deriv/shared';
 import { init } from 'Utils/server_time';
 import CashierProviders from './cashier-providers';
 import AppContent from './app-content';
+import { TWebSocket } from 'Types';
+import type { TRootStore } from '@deriv/stores/types';
 
-const App = ({ passthrough: { WS, root_store } }) => {
+type TAppProps = {
+    passthrough: {
+        WS: TWebSocket;
+        root_store: TRootStore;
+    };
+};
+
+const App = ({ passthrough: { WS, root_store } }: TAppProps) => {
     React.useEffect(() => {
         setWebsocket(WS);
         init();
