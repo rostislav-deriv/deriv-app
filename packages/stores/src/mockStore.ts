51dae5811d29306e187cf7b6a1da711e99d6bbe3
Rostik Kayko <rostislav@deriv.com>
2023-04-11 13:11:46 +0300

    refactor: fixed mockStore

--- packages/stores/src/mockStore.ts
+++ packages/stores/src/mockStore.ts
@@ -4,6 +4,7 @@ import type { TRootStore } from '../types';
 const mock = (): TRootStore => {
     return {
         client: {
+            account_settings: {},
             accounts: {
                 loginid: {
                     account_type: 'trading',
@@ -242,6 +243,7 @@ const mock = (): TRootStore => {
             sub_section_index: 0,
             toggleReadyToDepositModal: jest.fn(),
             is_ready_to_deposit_modal_visible: false,
+            is_real_acc_signup_on: false,
             is_need_real_account_for_cashier_modal_visible: false,
             toggleNeedRealAccountForCashierModal: jest.fn(),
         },
