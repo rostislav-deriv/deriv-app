361b1990d14309dcabd892da1adb52ea7b9e7d92
Rostik Kayko <rostislav@deriv.com>
2023-04-12 15:32:23 +0300

    refactor: a minor change

--- packages/cashier/src/stores/__tests__/payment-agent-store.spec.tsx
+++ packages/cashier/src/stores/__tests__/payment-agent-store.spec.tsx
@@ -239,36 +239,34 @@ describe('PaymentAgentStore', () => {
     it('should filter payment agent list by selected bank', async () => {
         await payment_agent_store.setPaymentAgentList();
         payment_agent_store.filterPaymentAgentList('card');
-        expect(payment_agent_store.filtered_list).toEqual([
-            {
-                currency: 'USD',
-                deposit_commission: '0',
-                email: 'pa@example.com',
-                further_information: 'further information',
-                max_withdrawal: '2000',
-                min_withdrawal: '10',
-                name: 'Payment Agent of CR90000000',
-                paymentagent_loginid: 'CR90000000',
-                phone_numbers: [{ phone_number: '+12345678' }],
-                supported_banks: [{ payment_method: 'Visa' }],
-                urls: [{ url: 'http://www.pa.com' }],
-                withdrawal_commission: '0',
-            },
-            {
-                currency: 'USD',
-                deposit_commission: '0',
-                email: 'pa@example.com',
-                further_information: 'further information',
-                max_withdrawal: '2000',
-                min_withdrawal: '10',
-                name: 'Payment Agent of CR90000002',
-                paymentagent_loginid: 'CR90000002',
-                phone_numbers: [{ phone_number: '+12345678' }],
-                supported_banks: [{ payment_method: 'Visa' }, { payment_method: 'Mastercard' }],
-                urls: [{ url: 'http://www.pa.com' }],
-                withdrawal_commission: '0',
-            },
-        ]);
+        expect(payment_agent_store.filtered_list).toContainEqual({
+            currency: 'USD',
+            deposit_commission: '0',
+            email: 'pa@example.com',
+            further_information: 'further information',
+            max_withdrawal: '2000',
+            min_withdrawal: '10',
+            name: 'Payment Agent of CR90000000',
+            paymentagent_loginid: 'CR90000000',
+            phone_numbers: [{ phone_number: '+12345678' }],
+            supported_banks: [{ payment_method: 'Visa' }],
+            urls: [{ url: 'http://www.pa.com' }],
+            withdrawal_commission: '0',
+        });
+        expect(payment_agent_store.filtered_list).toContainEqual({
+            currency: 'USD',
+            deposit_commission: '0',
+            email: 'pa@example.com',
+            further_information: 'further information',
+            max_withdrawal: '2000',
+            min_withdrawal: '10',
+            name: 'Payment Agent of CR90000002',
+            paymentagent_loginid: 'CR90000002',
+            phone_numbers: [{ phone_number: '+12345678' }],
+            supported_banks: [{ payment_method: 'Visa' }, { payment_method: 'Mastercard' }],
+            urls: [{ url: 'http://www.pa.com' }],
+            withdrawal_commission: '0',
+        });
     });
 
     it('should filter payment agent list by search term', async () => {
