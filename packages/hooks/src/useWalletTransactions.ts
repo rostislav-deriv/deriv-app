f9b9afddea85ed8e9f91ba12512b4e271d8b2cb0
Rostik Kayko <rostislav@deriv.com>
2023-07-17 16:32:58 +0300

    refactor: reduced and refactored the hook

--- packages/hooks/src/useWalletTransactions.ts
+++ packages/hooks/src/useWalletTransactions.ts
@@ -69,25 +69,25 @@ const useWalletTransactions = (
         () =>
             wallets && current_wallet
                 ? transactions
-                      .map((transaction: typeof transactions[number]) => {
-                          if (
-                              transaction.amount === undefined ||
-                              transaction.balance_after === undefined ||
-                              transaction.action_type === undefined
-                          )
-                              return null;
+                      // Filter out transactions with missing data.
+                      .filter(
+                          (
+                              transaction: typeof transactions[number]
+                          ): transaction is Omit<
+                              typeof transactions[number],
+                              'action_type' | 'amount' | 'balance_after'
+                          > & {
+                              action_type: Exclude<typeof transactions[number]['action_type'], undefined>;
+                              amount: Exclude<typeof transactions[number]['amount'], undefined>;
+                              balance_after: Exclude<typeof transactions[number]['balance_after'], undefined>;
+                          } =>
+                              transaction.action_type !== undefined &&
+                              transaction.amount !== undefined &&
+                              transaction.balance_after !== undefined
+                      )
+                      .map(transaction => {
+                          if (typeof transaction.action_type === 'undefined') return null;
 
-                          let account_category = 'wallet';
-                          let account_type = current_wallet.account_type;
-                          let account_name = `${current_wallet.is_virtual ? 'Demo ' : ''}${
-                              current_wallet.currency
-                          } Wallet`;
-                          let account_currency = current_wallet.currency;
-                          let gradient_class = current_wallet.gradient_card_class;
-                          let icon = getWalletCurrencyIcon(
-                              current_wallet.is_virtual ? 'demo' : current_wallet.currency || 'USD',
-                              is_dark_mode_on
-                          );
                           if (transaction.action_type === 'transfer') {
                               const other_loginid =
                                   transaction.to?.loginid === loginid
@@ -96,53 +96,51 @@ const useWalletTransactions = (
                               if (!other_loginid) return null;
                               const other_account = accounts.find(el => el?.loginid === other_loginid);
                               if (!other_account || !other_account.currency || !other_account.account_type) return null;
-                              account_category = other_account.account_category || 'wallet';
-                              account_currency = other_account.currency;
-                              account_name =
-                                  other_account.account_category === 'wallet'
-                                      ? `${
-                                            (
-                                                wallets.find(
-                                                    el => el.loginid === other_account.loginid
-                                                ) as typeof wallets[number]
-                                            ).is_virtual
-                                                ? 'Demo '
-                                                : ''
-                                        }${
-                                            (
-                                                wallets.find(
-                                                    el => el.loginid === other_account.loginid
-                                                ) as typeof wallets[number]
-                                            ).currency
-                                        } Wallet`
-                                      : getTradingAccountName(
-                                            other_account.account_type as 'standard' | 'mt5' | 'dxtrade' | 'binary',
-                                            !!other_account.is_virtual,
-                                            other_account.landing_company_shortcode as 'svg' | 'malta'
-                                        );
-                              account_type = other_account.account_type;
-                              gradient_class = `wallet-card__${
-                                  other_account.is_virtual === 1 ? 'demo' : other_account?.currency?.toLowerCase()
-                              }-bg${is_dark_mode_on ? '--dark' : ''}`;
-                              icon = getWalletCurrencyIcon(
-                                  other_account.is_virtual ? 'demo' : other_account.currency || '',
-                                  is_dark_mode_on,
-                                  false
-                              );
+                              const other_wallet = wallets.find(
+                                  el => el.loginid === other_account.loginid
+                              ) as typeof wallets[number];
+                              return {
+                                  ...transaction,
+                                  account_category: other_account.account_category,
+                                  account_currency: other_account.currency,
+                                  account_name:
+                                      other_account.account_category === 'wallet'
+                                          ? `${other_wallet.is_virtual ? 'Demo ' : ''}${other_wallet.currency} Wallet`
+                                          : getTradingAccountName(
+                                                other_account.account_type as 'standard' | 'mt5' | 'dxtrade' | 'binary',
+                                                !!other_account.is_virtual,
+                                                other_account.landing_company_shortcode
+                                            ),
+                                  account_type: other_account.account_type,
+                                  gradient_class: `wallet-card__${
+                                      other_account.is_virtual === 1 ? 'demo' : other_account?.currency?.toLowerCase()
+                                  }-bg${is_dark_mode_on ? '--dark' : ''}`,
+                                  icon: getWalletCurrencyIcon(
+                                      other_account.is_virtual ? 'demo' : other_account.currency || '',
+                                      is_dark_mode_on,
+                                      false
+                                  ),
+                                  icon_type:
+                                      getConfig(other_account.currency)?.is_crypto || current_wallet.is_virtual
+                                          ? 'crypto'
+                                          : 'fiat',
+                              };
                           }
-                          const currency_config = getConfig(account_currency || '');
-                          const is_crypto = currency_config?.is_crypto;
-                          const icon_type = is_crypto || current_wallet.is_virtual ? 'crypto' : 'fiat';
 
                           return {
                               ...transaction,
-                              account_category,
-                              account_currency,
-                              account_name,
-                              account_type,
-                              gradient_class,
-                              icon,
-                              icon_type,
+                              account_category: 'wallet',
+                              account_currency: current_wallet.currency,
+                              account_name: `${current_wallet.is_virtual ? 'Demo ' : ''}${
+                                  current_wallet.currency
+                              } Wallet`,
+                              account_type: current_wallet.account_type,
+                              gradient_class: current_wallet.gradient_card_class,
+                              icon: current_wallet.icon,
+                              icon_type:
+                                  current_wallet.currency_config?.is_crypto || current_wallet.is_virtual
+                                      ? 'crypto'
+                                      : 'fiat',
                           };
                       })
                       .filter(<T>(value: T | null): value is T => value !== null)
--- packages/utils/src/getTradingAccountName.ts
+++ packages/utils/src/getTradingAccountName.ts
@@ -7,13 +7,16 @@ const trading_accounts_display_prefixes = {
 
 const landing_company_display_shortcodes = {
     svg: 'SVG',
+    costarica: 'Costa Rica',
     malta: 'Malta',
+    maltainvest: 'Malta',
+    iom: 'IoM',
 } as const;
 
 const getTradingAccountName = (
     account_type: 'standard' | 'mt5' | 'dxtrade' | 'binary',
     is_virtual: boolean,
-    landing_company_shortcode: 'svg' | 'malta'
+    landing_company_shortcode: 'svg' | 'costarica' | 'maltainvest' | 'malta' | 'iom'
 ) => {
     return `${trading_accounts_display_prefixes[account_type]} ${
         is_virtual ? 'Demo' : `(${landing_company_display_shortcodes[landing_company_shortcode]})`
